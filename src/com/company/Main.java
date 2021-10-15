93009e01fdad99ade49be937dfa09437f0eac1be
NI210248 <NI210248@reigate.ac.uk>
2021-10-15 10:59:02 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Random rand = new Random();
+        int random = rand.nextInt(10) + 1; /* 1 <= x <= 10 */
+
+        System.out.println("_______");
+        System.out.println("|  |   ");
+        System.out.println("|  0   ");
+        System.out.println("| /|\\  ");
+        System.out.println("| / \\  ");
+        System.out.println("|      ");
+        System.out.println("|      ");
+
+        //System.out.println(random); // testing to see if random works
+
+        if (random = 0) {
+            
+        }
+    }
+}
